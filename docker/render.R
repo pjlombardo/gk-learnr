afc454310504aeebfbbbf76648e4191f770adf0a
pjlombardo <philip.j.lombardo@gmail.com>
2023-12-02 10:18:55 -0500

    removed docker stuff, doing it a different way

--- docker/render.R
+++
@@ -1,4 +0,0 @@
-print("Rendering LearnR Rmd-files")
-
-df<-read.table("render.txt")
-sapply(df$V1, function(x){rmarkdown::render(x)})
--- docker/render_learnr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /scripts
-
-find /srv/shiny-server -name *.Rmd >> render.txt
-
-# Add something to send files to a log file?
-
-Rscript render.R 
